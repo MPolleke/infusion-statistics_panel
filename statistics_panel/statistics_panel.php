77c1865637af29f83ac57aab7d6e479fe51efd26
Marcel Pol <marcel@timelord.nl>
2019-12-21 23:23:55 +0100

    Improve formatting. Add "Actief deze maand". Remove unused query.

--- statistics_panel/statistics_panel.php
+++ statistics_panel/statistics_panel.php
@@ -15,19 +15,25 @@
 | copyright header is strictly prohibited without
 | written permission from the original author(s).
 +--------------------------------------------------------*/
-if (!defined("IN_FUSION")) { die("Access Denied"); }
+if ( ! defined("IN_FUSION")) { die("Access Denied"); }
 
 
-$result = dbquery("SELECT u.user_name AS username, u.user_id AS userid, u.user_posts AS posts FROM ".DB_USERS." u ORDER BY posts DESC LIMIT 0,1");
-$mostactive = dbarray($result);
+//$result = dbquery("SELECT u.user_name AS username, u.user_id AS userid, u.user_posts AS posts FROM ".DB_USERS." u ORDER BY posts DESC LIMIT 0,1");
+//$mostactive = dbarray($result);
+
 openside('Statistieken');
-echo THEME_BULLET . ' Actieve Leden: ' . dbcount("(user_id)", DB_USERS, "user_status ='0'") . '<br />';
-echo THEME_BULLET . ' Gebande Leden: ' . dbcount("(user_id)", DB_USERS, "user_status ='1'") . '<br />';
-echo THEME_BULLET . ' Gedeactiveerde leden: ' . dbcount("(user_id)", DB_USERS, "user_status ='7'") . '<br />';
-//echo THEME_BULLET . ' Meest actief: ' . $mostactive['username'] . " (" . $mostactive['posts'] . ")" . '<br />';
-echo THEME_BULLET . ' Reacties: ' . dbcount("(comment_id)", DB_COMMENTS) . '<br />';
-echo THEME_BULLET . ' Shouts: ' . dbcount("(shout_id)", DB_SHOUTBOX) . '<br />';
-echo THEME_BULLET . ' Topics: ' . dbcount("(thread_id)", DB_FORUM_THREADS) . '<br />';
-echo THEME_BULLET . ' Berichten: ' . dbcount("(post_id)", DB_FORUM_POSTS) . '<br />';
+
+echo THEME_BULLET . ' Actieve Leden: ' . dbcount("(user_id)", DB_USERS, "user_status ='0'") . '<br />' . "\n";
+echo THEME_BULLET . ' Gebande Leden: ' . dbcount("(user_id)", DB_USERS, "user_status ='1'") . '<br />' . "\n";
+echo THEME_BULLET . ' Gedeactiveerde leden: ' . dbcount("(user_id)", DB_USERS, "user_status ='7'") . '<br />' . "\n";
+//echo THEME_BULLET . ' Meest actief: ' . $mostactive['username'] . " (" . $mostactive['posts'] . ")" . '<br />' . "\n";
+echo THEME_BULLET . ' Reacties: ' . dbcount("(comment_id)", DB_COMMENTS) . '<br />' . "\n";
+echo THEME_BULLET . ' Shouts: ' . dbcount("(shout_id)", DB_SHOUTBOX) . '<br />' . "\n";
+echo THEME_BULLET . ' Topics: ' . dbcount("(thread_id)", DB_FORUM_THREADS) . '<br />' . "\n";
+echo THEME_BULLET . ' Berichten: ' . dbcount("(post_id)", DB_FORUM_POSTS) . '<br />' . "\n";
 echo THEME_BULLET . ' Foto&apos;s: ' . dbcount("(photo_id)", DB_PHOTOS) . '<br />' . "\n";
+
+$month_ago = strtotime( '-1 month', time() );
+echo THEME_BULLET . ' Actief deze maand: ' . dbcount("(user_id)", DB_USERS, "user_lastvisit > '" . $month_ago . "'") . '<br />' . "\n";
+
 closeside();
